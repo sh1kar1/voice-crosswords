3945b2454f4b2602f4d5fe07d2114ef0dd01c4d2
Nikita Kablov <nikita.kablov68@gmail.com>
2024-05-27 00:10:40 +0300

    chore: rename h/w to rows/cols

--- src/Crossword.ts
+++ src/Crossword.ts
@@ -8,16 +8,16 @@ interface Word {
 
 export default class Crossword {
     words: Word[];
-    h: number;
-    w: number;
+    rows: number;
+    cols: number;
     board: string[][];
     down_desc: string[];
     across_desc: string[];
 
     constructor(words: Word[]) {
         this.words = words;
-        this.h = 0;
-        this.w = 0;
+        this.rows = 0;
+        this.cols = 0;
         this.board = [];
         this.down_desc = [];
         this.across_desc = [];
@@ -29,18 +29,18 @@ export default class Crossword {
     private calc_size(): void {
         for (let word of this.words) {
             if (word.is_down) {
-                this.h = Math.max(this.h, word.y + word.text.length);
-                this.w = Math.max(this.w, word.x + 1);
+                this.rows = Math.max(this.rows, word.y + word.text.length);
+                this.cols = Math.max(this.cols, word.x + 1);
             } else {
-                this.h = Math.max(this.h, word.y + 1);
-                this.w = Math.max(this.w, word.x + word.text.length);
+                this.rows = Math.max(this.rows, word.y + 1);
+                this.cols = Math.max(this.cols, word.x + word.text.length);
             }
         }
     }
 
     private create_board(): void {
-        for (let i = 0; i < this.h; i++) {
-            this.board[i] = Array(this.w).fill(' ');
+        for (let i = 0; i < this.rows; i++) {
+            this.board[i] = Array(this.cols).fill(' ');
         }
         for (let word of this.words) {
             for (let i = 0; i < word.text.length; i++) {
@@ -63,16 +63,3 @@ export default class Crossword {
         }
     }
 }
-
-// import * as fs from 'fs';
-//
-// const crossword = new Crossword(JSON.parse(fs.readFileSync('./src/levels/1.json', 'utf-8')));
-//
-// console.log(`Size: ${crossword.h} x ${crossword.w}`);
-//
-// for (let i = 0; i < crossword.h; i++) {
-//     console.log(crossword.board[i].join(' '));
-// }
-//
-// console.log(`Down:\n${crossword.down_desc.join('\n')}`);
-// console.log(`Across:\n${crossword.across_desc.join('\n')}`);
