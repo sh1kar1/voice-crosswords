e783b05786d7534f96ab4ef178882eb0a8dfba32
Nikita Kablov <nikita.kablov68@gmail.com>
2024-05-27 00:12:39 +0300

    chore: use GlobalStyles.tsx instead of App.css

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        height: 100%;
+        margin: 0;
+    }
+`;
+
+export default GlobalStyle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { DeviceThemeProvider, SSRProvider } from '@salutejs/plasma-ui';
 import './index.css';
+import GlobalStyle from './GlobalStyles';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <DeviceThemeProvider>
+            <SSRProvider>
+                <GlobalStyle />
+                <App />
+            </SSRProvider>
+        </DeviceThemeProvider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
