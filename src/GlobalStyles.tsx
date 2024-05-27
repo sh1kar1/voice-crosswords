533588ae916f512e0e2a39897ffe42223a8144b8
Nikita Kablov <nikita.kablov68@gmail.com>
2024-05-27 04:08:59 +0300

    chore: CSS reset

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,9 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    html {
-        height: 100%;
+    * {
         margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        line-height: 1;
+    }
+
+    ul, ol {
+        list-style: none;
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+        display: block;
     }
 `;
 
