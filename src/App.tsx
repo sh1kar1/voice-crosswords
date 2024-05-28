7498e174b7462822ca1cd273b261be33d0bdb6d5
Nikita Kablov <nikita.kablov68@gmail.com>
2024-05-28 20:13:34 +0300

    refactor: ternary operators, iterators and generators

--- src/App.tsx
+++ src/App.tsx
@@ -11,64 +11,44 @@ const App: React.FC = () => {
     const [prev_move, set_prev_move] = useState(0);
 
     const input_refs = useRef<(HTMLInputElement | null)[][]>([]);
-    for (let row = 0; row < crossword.rows; row++) {
-        input_refs.current[row] = Array(crossword.cols).fill(0);
-    }
+    input_refs.current = Array.from({ length: crossword.rows }, () => Array(crossword.cols).fill(0));
 
-    const handle_board_change = (row: number, col: number, val: string) => {
-        const new_board = board.map((r, r_idx) => r_idx === row ? r.map((c, c_idx) => (c_idx === col ? val : c)) : r);
-        set_board(new_board);
-    };
-
-    const handle_key_down = (e: React.KeyboardEvent<HTMLInputElement>, row: number, col: number) => {
-        if (e.key === 'ArrowRight' || e.key === 'ArrowLeft' || e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'Backspace') {
-            e.preventDefault();
-        }
-        if (e.key === 'ArrowRight' && col < crossword.cols - 1) {
+    const handle_key_down = (key: string, row: number, col: number) => {
+        if (key === 'ArrowRight' && col < crossword.cols - 1) {
             input_refs.current[row][col + 1]?.focus();
-        } else if (e.key === 'ArrowLeft' && col > 0) {
+        } else if (key === 'ArrowLeft' && col > 0) {
             input_refs.current[row][col - 1]?.focus();
-        } else if (e.key === 'ArrowDown' && row < crossword.rows - 1) {
+        } else if (key === 'ArrowDown' && row < crossword.rows - 1) {
             input_refs.current[row + 1][col]?.focus();
-        } else if (e.key === 'ArrowUp' && row > 0) {
+        } else if (key === 'ArrowUp' && row > 0) {
             input_refs.current[row - 1][col]?.focus();
-        } else if (e.key === 'Backspace') {
-            handle_board_change(row, col, '');
-            if (focused[0] !== 0 && row > 0 && crossword.board[row - 1][col] !== ' ' && crossword.words[focused[0] - 1].is_down
-                && focused[1] !== 0 && col > 0 && crossword.board[row][col - 1] !== ' ' && !crossword.words[focused[1] - 1].is_down) {
-                if (prev_move === 0) {
-                    input_refs.current[row - 1][col]?.focus();
-                } else {
-                    input_refs.current[row][col - 1]?.focus();
-                }
-            } else {
-                if (focused[0] !== 0 && row > 0 && crossword.board[row - 1][col] !== ' ' && crossword.words[focused[0] - 1].is_down) {
-                    input_refs.current[row - 1][col]?.focus();
-                    set_prev_move(0);
-                }
-                if (focused[1] !== 0 && col > 0 && crossword.board[row][col - 1] !== ' ' && !crossword.words[focused[1] - 1].is_down) {
-                    input_refs.current[row][col - 1]?.focus();
-                    set_prev_move(1);
-                }
+
+        } else if (key === 'Backspace') {
+            set_board(board.map((r, r_idx) => r_idx === row ? r.map((c, c_idx) => (c_idx === col ? '' : c)) : r));
+            const move_up = focused[0] !== 0 && row > 0 && crossword.board[row - 1][col] !== ' ' && crossword.words[focused[0] - 1].is_down;
+            const move_left = focused[1] !== 0 && col > 0 && crossword.board[row][col - 1] !== ' ' && !crossword.words[focused[1] - 1].is_down;
+            if (move_up && move_left) {
+                prev_move === 0 ? input_refs.current[row - 1][col]?.focus() : input_refs.current[row][col - 1]?.focus();
+            } else if (move_up) {
+                input_refs.current[row - 1][col]?.focus();
+                set_prev_move(0);
+            } else if (move_left) {
+                input_refs.current[row][col - 1]?.focus();
+                set_prev_move(1);
             }
-        } else if ((/^[a-zа-я]*$/).test(e.key)) {
-            handle_board_change(row, col, e.key);
-            if (focused[0] !== 0 && row < crossword.rows - 1 && crossword.board[row + 1][col] !== ' ' && crossword.words[focused[0] - 1].is_down
-                && focused[1] !== 0 && col < crossword.cols - 1 && crossword.board[row][col + 1] !== ' ' && !crossword.words[focused[1] - 1].is_down) {
-                if (prev_move === 0) {
-                    input_refs.current[row + 1][col]?.focus();
-                } else {
-                    input_refs.current[row][col + 1]?.focus();
-                }
-            } else {
-                if (focused[0] !== 0 && row < crossword.rows - 1 && crossword.board[row + 1][col] !== ' ' && crossword.words[focused[0] - 1].is_down) {
-                    input_refs.current[row + 1][col]?.focus();
-                    set_prev_move(0);
-                }
-                if (focused[1] !== 0 && col < crossword.cols - 1 && crossword.board[row][col + 1] !== ' ' && !crossword.words[focused[1] - 1].is_down) {
-                    input_refs.current[row][col + 1]?.focus();
-                    set_prev_move(1);
-                }
+
+        } else if ((/^[a-zа-я]*$/).test(key)) {
+            set_board(board.map((r, r_idx) => r_idx === row ? r.map((c, c_idx) => (c_idx === col ? key : c)) : r));
+            const move_down = focused[0] !== 0 && row < crossword.rows - 1 && crossword.board[row + 1][col] !== ' ' && crossword.words[focused[0] - 1].is_down;
+            const move_right = focused[1] !== 0 && col < crossword.cols - 1 && crossword.board[row][col + 1] !== ' ' && !crossword.words[focused[1] - 1].is_down;
+            if (move_down && move_right) {
+                prev_move === 0 ? input_refs.current[row + 1][col]?.focus() : input_refs.current[row][col + 1]?.focus();
+            } else if (move_down) {
+                input_refs.current[row + 1][col]?.focus();
+                set_prev_move(0);
+            } else if (move_right) {
+                input_refs.current[row][col + 1]?.focus();
+                set_prev_move(1);
             }
         }
     };
@@ -98,9 +78,9 @@ const App: React.FC = () => {
                                         value={cell}
                                         maxLength={1}
                                         ref={(el) => input_refs.current[row_idx][col_idx] = el}
-                                        onKeyDown={(e) => handle_key_down(e, row_idx, col_idx)}
                                         onFocus={() => set_focused(crossword.words_idx[row_idx][col_idx])}
                                         onBlur={() => set_focused([0, 0])}
+                                        onKeyDown={(e) => { e.preventDefault(); handle_key_down(e.key, row_idx, col_idx); }}
                                     />
                                 </Cell>
                             ))}
--- src/Crossword.ts
+++ src/Crossword.ts
@@ -20,89 +20,56 @@ export default class Crossword {
 
     constructor(words: Word[]) {
         this.words = words;
+
         this.rows = 0;
         this.cols = 0;
-        this.board = [];
-        this.empty_board = [];
+        this.calc_size();
+
+        this.board = Array.from({ length: this.rows }, () => Array(this.cols).fill(' '));
+        this.empty_board = Array.from({ length: this.rows }, () => Array(this.cols).fill(' '));
+        this.create_board();
+
         this.down_desc = [];
         this.across_desc = [];
-        this.words_idx = [];
-        this.words_start_idx = [];
-        this.desc_to_idx = [];
-        this.calc_size();
-        this.create_board();
+        this.desc_to_idx = Array.from({ length: 2 }, () => Array(this.words.length).fill(0));
         this.create_description();
+
+        this.words_idx = Array.from({ length: this.rows }, () => Array.from({ length: this.cols }, () => Array(2).fill(0)));
+        this.words_start_idx = Array.from({ length: this.rows }, () => Array(this.cols).fill(0));
         this.create_words_idx();
     }
 
     private calc_size(): void {
         for (let word of this.words) {
-            if (word.is_down) {
-                this.rows = Math.max(this.rows, word.row + word.text.length);
-                this.cols = Math.max(this.cols, word.col + 1);
-            } else {
-                this.rows = Math.max(this.rows, word.row + 1);
-                this.cols = Math.max(this.cols, word.col + word.text.length);
-            }
+            this.rows = word.is_down ? Math.max(this.rows, word.row + word.text.length) : Math.max(this.rows, word.row + 1);
+            this.cols = word.is_down ? Math.max(this.cols, word.col + 1) : Math.max(this.cols, word.col + word.text.length);
         }
     }
 
     private create_board(): void {
-        for (let row = 0; row < this.rows; row++) {
-            this.board[row] = Array(this.cols).fill(' ');
-            this.empty_board[row] = Array(this.cols).fill(' ');
-        }
         for (let word of this.words) {
             for (let i = 0; i < word.text.length; i++) {
-                if (word.is_down) {
-                    this.board[word.row + i][word.col] = word.text[i];
-                } else {
-                    this.board[word.row][word.col + i] = word.text[i];
-                }
-            }
-        }
-        for (let row = 0; row < this.rows; row++) {
-            for (let col = 0; col < this.cols; col++) {
-                if (this.board[row][col] !== ' ') {
-                    this.empty_board[row][col] = '';
-                }
+                this.board[word.row + (word.is_down ? i : 0)][word.col + (!word.is_down ? i : 0)] = word.text[i];
+                this.empty_board[word.row + (word.is_down ? i : 0)][word.col + (!word.is_down ? i : 0)] = '';
             }
         }
     }
 
     private create_description(): void {
-        this.desc_to_idx[0] = Array(this.words.length).fill(0);
-        this.desc_to_idx[1] = Array(this.words.length).fill(0);
-        for (let i = 0; i < this.words.length; i++) {
-            if (this.words[i].is_down) {
-                this.down_desc.push(`${i + 1}.${i + 1 < 10 ? '   ' : ' '}${this.words[i].desc}`);
-                this.desc_to_idx[0][this.down_desc.length - 1] = i + 1;
-            } else {
-                this.across_desc.push(`${i + 1}.${i + 1 < 10 ? '   ' : ' '}${this.words[i].desc}`);
-                this.desc_to_idx[1][this.across_desc.length - 1] = i + 1;
-            }
+        for (let [i, word] of this.words.entries()) {
+            (word.is_down ? this.down_desc : this.across_desc).push(`${i + 1}.${i + 1 < 10 ? '   ' : ' '}${word.desc}`);
+            this.desc_to_idx[word.is_down ? 0 : 1][(word.is_down ? this.down_desc : this.across_desc).length - 1] = i + 1;
         }
     }
 
     private create_words_idx(): void {
         for (let row = 0; row < this.rows; row++) {
-            this.words_start_idx[row] = Array(this.cols).fill(0);
-            this.words_idx[row] = Array(this.cols);
             for (let col = 0; col < this.cols; col++) {
-                this.words_idx[row][col] = [0, 0];
-            }
-        }
-        for (let row = 0; row < this.rows; row++) {
-            for (let col = 0; col < this.cols; col++) {
-                for (let i = 0; i < this.words.length; i++) {
-                    if (this.words[i].row === row && this.words[i].col === col) {
+                for (let [i, word] of this.words.entries()) {
+                    if (word.row === row && word.col === col) {
                         this.words_start_idx[row][col] = i + 1;
-                        for (let j = 0; j < this.words[i].text.length; j++) {
-                            if (this.words[i].is_down) {
-                                this.words_idx[row + j][col][0] = i + 1;
-                            } else {
-                                this.words_idx[row][col + j][1] = i + 1;
-                            }
+                        for (let j = 0; j < word.text.length; j++) {
+                            this.words_idx[row + (word.is_down ? j : 0)][col + (!word.is_down ? j : 0)][word.is_down ? 0 : 1] = i + 1;
                         }
                     }
                 }
