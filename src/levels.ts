413887daebf7254d416b6598984731d95a6bd369
Nikita Kablov <nikita.kablov68@gmail.com>
2024-05-29 08:01:13 +0300

    feat: add 1 level

--- src/levels.ts
+++ src/levels.ts
@@ -1,5 +1,5 @@
-const l1 = [
-    {
+export const l0 = [
+    {   "n": 1,
         "text": "watch",
         "desc": "You wear a _ to know the time",
         "col": 1,
@@ -7,6 +7,7 @@ const l1 = [
         "is_down": false
     },
     {
+        "n": 2,
         "text": "hands",
         "desc": "A clock has two _",
         "col": 5,
@@ -14,6 +15,7 @@ const l1 = [
         "is_down": true
     },
     {
+        "n": 3,
         "text": "years",
         "desc": "A century has one hundred _",
         "col": 11,
@@ -21,6 +23,7 @@ const l1 = [
         "is_down": true
     },
     {
+        "n": 4,
         "text": "days",
         "desc": "There are seven _ in a week",
         "col": 5,
@@ -28,6 +31,7 @@ const l1 = [
         "is_down": false
     },
     {
+        "n": 5,
         "text": "seconds",
         "desc": "A minute has sixty _",
         "col": 8,
@@ -35,6 +39,7 @@ const l1 = [
         "is_down": true
     },
     {
+        "n": 6,
         "text": "months",
         "desc": "A year has twelve _",
         "col": 0,
@@ -42,6 +47,7 @@ const l1 = [
         "is_down": false
     },
     {
+        "n": 7,
         "text": "time",
         "desc": "A clock helps to know the _",
         "col": 3,
@@ -49,6 +55,7 @@ const l1 = [
         "is_down": true
     },
     {
+        "n": 8,
         "text": "calendar",
         "desc": "It helps you to know the days of the week",
         "col": 5,
@@ -56,6 +63,7 @@ const l1 = [
         "is_down": true
     },
     {
+        "n": 9,
         "text": "hours",
         "desc": "A day has twenty four _",
         "col": 7,
@@ -63,6 +71,7 @@ const l1 = [
         "is_down": false
     },
     {
+        "n": 10,
         "text": "weeks",
         "desc": "A year has fifty two _",
         "col": 4,
@@ -71,4 +80,325 @@ const l1 = [
     }
 ]
 
-export { l1 };
+export const l1 = [
+    {
+        "n": 1,
+        "text": "слепыш",
+        "desc": "Незрячий грызун, но не крот",
+        "col": 0,
+        "row": 0,
+        "is_down": true
+    },
+    {
+        "n": 1,
+        "text": "силач",
+        "desc": "Гнёт подкову, как калач",
+        "col": 0,
+        "row": 0,
+        "is_down": false
+    },
+    {
+        "n": 2,
+        "text": "лосины",
+        "desc": "Кожаные колготки бравого гусара",
+        "col": 2,
+        "row": 0,
+        "is_down": true
+    },
+    {
+        "n": 3,
+        "text": "чушь",
+        "desc": "Глупость, что можно городить",
+        "col": 4,
+        "row": 0,
+        "is_down": true
+    },
+    {
+        "n": 4,
+        "text": "клоака",
+        "desc": "Канализация Древнего Рима",
+        "col": 6,
+        "row": 0,
+        "is_down": true
+    },
+    {
+        "n": 5,
+        "text": "удавка",
+        "desc": "Узел, завязанный боцманом",
+        "col": 8,
+        "row": 0,
+        "is_down": true
+    },
+    {
+        "n": 6,
+        "text": "саго",
+        "desc": "Крупа из сердцевины пальмы",
+        "col": 10,
+        "row": 0,
+        "is_down": true
+    },
+    {
+        "n": 6,
+        "text": "стояк",
+        "desc": "Вертикальный брус, служащий опорой",
+        "col": 10,
+        "row": 0,
+        "is_down": false
+    },
+    {
+        "n": 7,
+        "text": "оконце",
+        "desc": "Смотровое отверстие в деревенской избе",
+        "col": 12,
+        "row": 0,
+        "is_down": true
+    },
+    {
+        "n": 8,
+        "text": "квакша",
+        "desc": "Лягушка, сидящая на ветке",
+        "col": 14,
+        "row": 0,
+        "is_down": true
+    },
+    {
+        "n": 9,
+        "text": "укладка",
+        "desc": "Процесс обретения причёской формы",
+        "col": 4,
+        "row": 1,
+        "is_down": false
+    },
+    {
+        "n": 10,
+        "text": "паинька",
+        "desc": "Ребёнок, не создающий проблем родителям",
+        "col": 0,
+        "row": 3,
+        "is_down": false
+    },
+    {
+        "n": 11,
+        "text": "вторник",
+        "desc": "Первый лёгкий день недели",
+        "col": 8,
+        "row": 3,
+        "is_down": false
+    },
+    {
+        "n": 12,
+        "text": "шлык",
+        "desc": "Колпак казачьей папахи",
+        "col": 0,
+        "row": 5,
+        "is_down": false
+    },
+    {
+        "n": 13,
+        "text": "лавка",
+        "desc": "Супермаркет в сельской местности",
+        "col": 1,
+        "row": 5,
+        "is_down": true
+    },
+    {
+        "n": 14,
+        "text": "кулик",
+        "desc": "Патриотическая болотная птица",
+        "col": 3,
+        "row": 5,
+        "is_down": true
+    },
+    {
+        "n": 15,
+        "text": "затон",
+        "desc": "Природное образование на реке",
+        "col": 5,
+        "row": 5,
+        "is_down": true
+    },
+    {
+        "n": 15,
+        "text": "завал",
+        "desc": "Преграда для спелеолога",
+        "col": 5,
+        "row": 5,
+        "is_down": false
+    },
+    {
+        "n": 16,
+        "text": "леший",
+        "desc": "Дух из чащи, недолюбливающий людей",
+        "col": 9,
+        "row": 5,
+        "is_down": true
+    },
+    {
+        "n": 17,
+        "text": "вирус",
+        "desc": "Опасная компьютерная зараза",
+        "col": 11,
+        "row": 5,
+        "is_down": true
+    },
+    {
+        "n": 17,
+        "text": "вега",
+        "desc": "Самая яркая звезда на северном небе",
+        "col": 11,
+        "row": 5,
+        "is_down": false
+    },
+    {
+        "n": 18,
+        "text": "гуппи",
+        "desc": "Аквариумная порода рыб",
+        "col": 13,
+        "row": 5,
+        "is_down": true
+    },
+    {
+        "n": 19,
+        "text": "валет",
+        "desc": "Юнец, побитый дамой",
+        "col": 1,
+        "row": 7,
+        "is_down": false
+    },
+    {
+        "n": 20,
+        "text": "шуруп",
+        "desc": "Гвоздь, который закручивают",
+        "col": 9,
+        "row": 7,
+        "is_down": false
+    },
+    {
+        "n": 21,
+        "text": "бакшиш",
+        "desc": "Взятка для татарина",
+        "col": 0,
+        "row": 9,
+        "is_down": true
+    },
+    {
+        "n": 21,
+        "text": "барк",
+        "desc": "Судно с косыми парусами",
+        "col": 0,
+        "row": 9,
+        "is_down": false
+    },
+    {
+        "n": 22,
+        "text": "ролики",
+        "desc": "Коньки для тёплой погоды",
+        "col": 2,
+        "row": 9,
+        "is_down": true
+    },
+    {
+        "n": 23,
+        "text": "нищий",
+        "desc": "Хозяин котомки на паперти",
+        "col": 5,
+        "row": 9,
+        "is_down": false
+    },
+    {
+        "n": 24,
+        "text": "иранец",
+        "desc": "Абориген современной Персии",
+        "col": 6,
+        "row": 9,
+        "is_down": true
+    },
+    {
+        "n": 25,
+        "text": "ирония",
+        "desc": "Родная сестра сарказма",
+        "col": 8,
+        "row": 9,
+        "is_down": true
+    },
+    {
+        "n": 26,
+        "text": "слип",
+        "desc": "Наклонная площадка сейнера",
+        "col": 11,
+        "row": 9,
+        "is_down": false
+    },
+    {
+        "n": 27,
+        "text": "латник",
+        "desc": "Рыцарь в тяжёлых доспехах",
+        "col": 12,
+        "row": 9,
+        "is_down": true
+    },
+    {
+        "n": 28,
+        "text": "пиетет",
+        "desc": "Глубокое уважение, благоговение",
+        "col": 14,
+        "row": 9,
+        "is_down": true
+    },
+    {
+        "n": 29,
+        "text": "колядка",
+        "desc": "Песня, исполняемая на Святки",
+        "col": 0,
+        "row": 11,
+        "is_down": false
+    },
+    {
+        "n": 30,
+        "text": "джут",
+        "desc": "Из него в индии вьют верёвки",
+        "col": 4,
+        "row": 11,
+        "is_down": true
+    },
+    {
+        "n": 31,
+        "text": "отбытие",
+        "desc": "Начало путешествия устами бывалого вояки",
+        "col": 8,
+        "row": 11,
+        "is_down": false
+    },
+    {
+        "n": 32,
+        "text": "боец",
+        "desc": "Тот, кто активно борется за победу",
+        "col": 10,
+        "row": 11,
+        "is_down": true
+    },
+    {
+        "n": 33,
+        "text": "убежище",
+        "desc": "Оно обычно находится под землёй",
+        "col": 4,
+        "row": 13,
+        "is_down": false
+    },
+    {
+        "n": 34,
+        "text": "штифт",
+        "desc": "Крепёжный стержень",
+        "col": 0,
+        "row": 14,
+        "is_down": false
+    },
+    {
+        "n": 35,
+        "text": "цукат",
+        "desc": "Плод, сваренный в сахарном сиропе",
+        "col": 10,
+        "row": 14,
+        "is_down": false
+    }
+]
